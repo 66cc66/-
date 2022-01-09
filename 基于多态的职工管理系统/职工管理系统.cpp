db41884381e3d6fab8203e8da7e67f302d407557
66cc66 <90556550+66cc66@users.noreply.github.com>
2022-01-09 18:34:40 +0800

    Update 职工管理系统.cpp

--- 基于多态的职工管理系统/职工管理系统.cpp
+++ 基于多态的职工管理系统/职工管理系统.cpp
@@ -3,42 +3,66 @@ using namespace std;
 #include"workManager.h"
 #include "work.h"
 #include "employee.h"
+#include "boss.h"
+#include "manager.h"
+
 int main()
 {
-	//workManager wm;
-	//int choice;
-	//do
-	//{
-	//	wm.show_Menue();
-	//	cout << "请输入您的选择\n";
-	//	cin >> choice;
-	//	switch (choice)
-	//	{
-	//	case 0:
-	//		wm.exitSystem();
-	//		break;
-	//	case 1:
-	//		break;
-	//	case 2:
-	//		break;
-	//	case 3:
-	//		break;
-	//	case 4:
-	//		break;
-	//	case 5:
-	//		break;
-	//	case 6:
-	//		break;
-	//	case 7:
-	//		break;
-	//	default:
-	//		system("cls");
-	//		break;//清屏
-	//	}
-	//} while (choice);
+	workManager wm;
+	int choice;
+	do
+	{
+		wm.show_Menue();
+		cout << "璇疯緭鍏ユ偍鐨勯�夋嫨\n";
+		cin >> choice;
+		switch (choice)
+		{
+		case 0:
+			wm.exitSystem();
+			break;
+		case 1:
+			wm.Add_Emp();
+			break;
+		case 2:
+			wm.show_Emp();
+			break;
+		case 3:
+			wm.del_Emp();
+			break;
+		case 4:
+			wm.Mod_Emp();
+			break;
+		case 5:
+			wm.Find_Emp();
+			break;
+		case 6:
+			wm.Sort_Emp();
+			break;
+		case 7:
+			wm.Clean_file();
+			break;
+		default:
+			system("cls");
+			break;//娓呭睆
+		}
+	} while (choice);
 
-	Worker* worker = NULL;
-	worker = new Employee(1, "张三", 1);//父类指针指向子类对象
-	worker->showInfo();
+	//Worker* worker1 = NULL;
+	//worker1 = new Employee(1, "寮犱笁", 1);//鐖剁被鎸囬拡鎸囧悜瀛愮被瀵硅薄
+	//worker1->showInfo();
+	//
+	//Worker* worker2 = NULL;
+	//worker2 = new Boss(2, "鏉庡洓", 2);
+	//worker2->showInfo();
+
+	//Worker* worker3 = NULL;
+	//worker3 = new Manager(3, "鐜嬩簲", 3);
+	//worker3->showInfo();
+
+	//delete worker1;
+	//delete worker2;
+	//delete worker3;
+
+	
 	return 0;
 }
