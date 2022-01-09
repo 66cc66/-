12f20c6c05e6264c4a3b0f98ef43bb5f8887009f
66cc66 <90556550+66cc66@users.noreply.github.com>
2022-01-09 18:30:39 +0800

    Update workManager.cpp

--- 基于多态的职工管理系统/workManager.cpp
+++ 基于多态的职工管理系统/workManager.cpp
@@ -54,6 +54,11 @@ workManager::~workManager()
 {
 	if (this->m_EmpArr != NULL)
 	{
+		for (int i = 0; i < this->m_EmpNum; i++)
+		{
+			if (this->m_EmpArr != NULL)
+				delete this->m_EmpArr[i];
+		}
 		delete[] this->m_EmpArr;
 		this->m_EmpArr = NULL;
 	}
@@ -80,6 +85,30 @@ void workManager::exitSystem()
 	cout << "欢迎下次继续使用\n";
 	system("pause");
 	exit(0);
+}
+
+void workManager::Ensure_consist(int Id)
+{
+	bool flag = false;
+	while (!flag)
+	{
+		for (int t = 0; t < this->get_EmpNum(); t++)
+		{
+			if (Id == this->m_EmpArr[t]->m_Id)
+			{
+				flag = true;
+				break;
+			}
+		}
+		if (flag)
+		{
+			cout << "所输编号已存在，请输入其他编号\n";
+			cin >> Id;
+			flag = false;
+		}
+		else
+			break;
+	}
 }
 
 void workManager::Add_Emp()
@@ -113,6 +142,9 @@ void workManager::Add_Emp()
 
 			cout << "请输入第" << i + 1 << "个新职工编号：\n";
 			cin >> Id;
+			
+			//保证Id的唯一性
+			Ensure_consist(Id);
 
 			cout << "请输入第" << i + 1 << "个新职工的姓名：\n";
 			cin >> name;
@@ -225,4 +257,301 @@ void workManager::init_Emp()
 		//存放在数组中
 		this->m_EmpArr[index++] = worker;
 	}
+}
+
+void workManager::show_Emp()
+{
+	if (this->m_FileIsEmpty)
+		cout << "文件不存在或为空！\n";
+	else
+	{
+		for (int i = 0; i < this->m_EmpNum; i++)
+		{
+			//利用多态调用接口
+			this->m_EmpArr[i]->showInfo();
+		}
+	}
+	system("pause");
+	system("cls");
+}
+
+void workManager::del_Emp()
+{
+	if (this->m_FileIsEmpty)
+		cout << "文件不存在或记录为空\n";
+	else
+	{
+		//按职工编号删除
+		cout << "输入所删职工的编号\n";
+		int id;
+		cin >> id;
+		
+		int index = this->Isexist(id);
+		if (index != -1)
+		{
+			//数据前移
+			for (int i = index; i < this->m_EmpNum - 1; i++)
+			{
+				this->m_EmpArr[i] = this->m_EmpArr[i + 1];
+			}
+			//更新数组中记录人员个数
+			this->m_EmpNum--;
+			//数据同步更新到文件中
+			this->save();
+
+			cout << "删除成功!\n";
+		}
+		else
+			cout << "删除失败, 未找到该职工\n";
+	}
+	system("pause");
+	system("cls");
+}
+
+int workManager::Isexist(int id)
+{
+	int index = -1;
+	for (int i = 0; i < this->m_EmpNum; i++)
+	{
+		if (id == this->m_EmpArr[i]->m_Id)
+		{
+			index = i;
+			break;
+		}
+	}
+	return index;
+}
+
+void workManager::Isexistname(string name)
+{
+	bool flag = false;
+	for (int i = 0; i < this->m_EmpNum; i++)
+	{
+		if (name == this->m_EmpArr[i]->m_Name)
+		{
+			cout << "查找成功，职工编号为："
+				<< this->m_EmpArr[i]->m_Id
+				<< "号的信息如下：\n";
+			this->m_EmpArr[i]->showInfo();
+			flag = true; 
+		}
+	}
+	if (!flag)
+		cout << "查无此人\n";
+}
+
+void workManager::Mod_Emp()
+{
+	if (this->m_FileIsEmpty)
+		cout << "文件不存在或记录为空\n";
+	else
+	{
+		cout << "输入所修改职工的编号\n";
+		int id;
+		cin >> id;
+
+		int ret = this->Isexist(id);
+		if (ret != -1)
+		{
+			//查找到了
+			delete this->m_EmpArr[ret];
+
+			int newid = 0;
+			string newname = " ";
+			int newselect = 0;
+
+			cout << "查到：" << id << "号职工，请重新输入该职工的信息\n" << "请输入编号\n";
+			cin >> newid;
+			this->Ensure_consist(newid);
+
+			cout << "请输入姓名：\n";
+			cin >> newname;
+
+			cout << "请输入岗位：\n";
+			cout << "1.普通职工\n";
+			cout << "2.经理\n";
+			cout << "3.老板\n";
+			cin >> newselect;
+
+			Worker* worker = NULL;
+			switch (newselect)
+			{
+			case 1:worker = new Employee(newid, newname, 1);
+				break;
+			case 2:worker = new Manager(newid, newname, 2);
+				break;
+			case 3:worker = new Boss(newid, newname, 3);
+				break;
+			default:
+				break;
+			}
+			//更新数据到数组中
+			this->m_EmpArr[ret] = worker;
+			if (this->m_EmpArr[ret] != NULL)
+			cout << "修改成功!\n";
+
+			//保存到文件
+			this->save();
+		}
+		else
+			cout << "修改失败，查无此人\n";
+	}
+	system("pause");
+	system("cls");
+}
+
+void workManager::Find_Emp()
+{
+	if (this->m_FileIsEmpty)
+		cout << "文件不存在或记录为空\n";
+	else
+	{
+		cout << "请选择查找方式\n"
+			<< "1. 按编号\n"
+			<< "2. 按姓名\n";
+		int select;
+		cin >> select;
+		if (select == 1)
+		{
+			int id;
+			cout << "请输入编号\n";
+			cin >> id;
+			int ret = this->Isexist(id);
+			if (ret != -1)
+				this->m_EmpArr[ret]->showInfo();
+			else
+				cout << "查无此人\n";
+		}
+		else if (select == 2)
+		{
+			string name;
+			cout << "请输入姓名\n";
+			cin >> name;
+
+			this->Isexistname(name);
+		}
+		else
+			cout << "输入有误\n";
+	}
+	system("pause");
+	system("cls");
+}
+
+void workManager::Sort_Emp()
+{
+	if (this->m_FileIsEmpty)
+		cout << "文件不存在或记录为空\n";
+	else
+	{
+		cout << "请选择排序方式\n"
+			<< "1.升序\n"
+			<< "2.降序\n";
+		int choice;
+		cin >> choice;
+		if (choice == 1)
+			ascend_order(0, this->m_EmpNum - 1, this->m_EmpArr);
+		else if (choice == 2)
+			descend_order(0, this->m_EmpNum - 1, this->m_EmpArr);
+		else
+		{
+			cout << "输入有误\n";
+			system("pause");
+			system("cls");
+		}
+		cout << "排序成功\n";
+		this->save();
+		//this->show_Emp();
+	}
+}
+
+void workManager::ascend_order(int left, int right, Worker** m_EmpArr)
+{
+	if (left >= right)
+		return;
+	int q = left, p = right;
+	while (q < p)
+	{
+		while (q < p && m_EmpArr[p]->m_Id >= m_EmpArr[left]->m_Id)
+			p--;
+		while (q < p && m_EmpArr[q]->m_Id <= m_EmpArr[left]->m_Id)
+			q++;
+
+		if (q == p)
+			break;
+		Worker* t = this->m_EmpArr[q];
+		m_EmpArr[q] = m_EmpArr[p];
+		m_EmpArr[p] = t;
+	}
+	if (q != left)
+	{
+		Worker* t = m_EmpArr[left];
+		m_EmpArr[left] = m_EmpArr[p];
+		m_EmpArr[p] = t;
+	}
+
+	ascend_order(left, q - 1, m_EmpArr);
+	ascend_order(p + 1, right, m_EmpArr);
+}
+
+void workManager::descend_order(int left, int right, Worker** m_EmpArr)
+{
+	if (left >= right)
+		return;
+	int q = left, p = right;
+	while (q < p)
+	{
+		while (q < p && m_EmpArr[p]->m_Id <= m_EmpArr[left]->m_Id)
+			p--;
+		while (q < p && m_EmpArr[q]->m_Id >= m_EmpArr[left]->m_Id)
+			q++;
+
+		if (q == p)
+			break;
+		Worker* t = this->m_EmpArr[q];
+		m_EmpArr[q] = m_EmpArr[p];
+		m_EmpArr[p] = t;
+	}
+	if (q != left)
+	{
+		Worker* t = m_EmpArr[left];
+		m_EmpArr[left] = m_EmpArr[p];
+		m_EmpArr[p] = t;
+	}
+
+	descend_order(left, q - 1, m_EmpArr);
+	descend_order(p + 1, right, m_EmpArr);
+}
+
+void workManager::Clean_file()
+{
+	cout << "确认情况吗？\n"
+		<< "1.确认\n"
+		<< "2.返回\n";
+
+	int choice;
+	cin >> choice;
+
+	if (choice == 1)
+	{
+		//打开模式ios::trunc,如果存在，删除并重新创建
+		ofstream ofs(FILENAME, ios::trunc);
+		ofs.close();
+
+		if (this->m_EmpArr != NULL)
+		{
+			for (int i = 0; i < this->m_EmpNum; i++)
+			{
+				if (this->m_EmpArr != NULL)
+					delete this->m_EmpArr[i];
+			}
+
+			this->m_EmpNum = 0;
+			delete[] this->m_EmpArr;
+			this->m_EmpArr = NULL;
+			this->m_FileIsEmpty = true;
+		}
+		cout << "清空成功\n";
+	}
+	system("pause");
+	system("cls");
 }
